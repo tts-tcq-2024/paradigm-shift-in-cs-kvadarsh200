7d1ae07dad6658c57eb83be73d684f7536ac75c3
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 13:57:36 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -2,75 +2,25 @@ using System;
 
 namespace ParadigmShift
 {
-    public enum StatusType
-    {
-        Normal,
-        Warning,
-        Breach
-    }
-
     public class checker
     {
-        private static StatusType GetStatus(float value, float lowerLimit, float upperLimit)
-        {
-            float lowerWarningThreshold = lowerLimit + upperLimit * GlobalSettings.WarningTolerance;
-            float upperWarningThreshold = upperLimit - upperLimit * GlobalSettings.WarningTolerance;
+        private readonly List<Parameter> parameters = new List<Parameter>();
 
-            if (value < lowerLimit || value > upperLimit)
+        public void AddParameter(Parameter parameter)
+        {
+            parameters.Add(parameter);
+        }
+
+        public bool CheckBattery(params float[] values)
+        {
+            bool isBatteryOk = true;
+
+            for (int i = 0; i < values.Length; i++)
             {
-                return StatusType.Breach;
+                isBatteryOk &= parameters[i].Check(values[i]);
             }
-            else if (value <= lowerWarningThreshold || value >= upperWarningThreshold)
-            {
-                return StatusType.Warning;
-            }
-            return StatusType.Normal;
-        }
 
-        public static StatusType CheckTemperatureStatus(float temperature)
-        {
-            return GetStatus(temperature, 0, 45);
-        }
-
-        public static StatusType CheckSocStatus(float soc)
-        {
-            return GetStatus(soc, 20, 80);
-        }
-
-        public static StatusType CheckChargeRateStatus(float chargeRate)
-        {
-            return GetStatus(chargeRate, 0, 0.8f);
-        }
-
-        public static void CheckTemperature(float temperature)
-        {
-            var status = CheckTemperatureStatus(temperature);
-            if (status != StatusType.Normal)
-                Console.WriteLine(MessageHelper.GetTemperatureMessage(status));
-        }
-
-        public static void CheckSoc(float soc)
-        {
-            var status = CheckSocStatus(soc);
-            if (status != StatusType.Normal)
-                Console.WriteLine(MessageHelper.GetSocMessage(status));
-        }
-
-        public static void CheckChargeRate(float chargeRate)
-        {
-            var status = CheckChargeRateStatus(chargeRate);
-            if (status != StatusType.Normal)
-                Console.WriteLine(MessageHelper.GetChargeRateMessage(status));
-        }
-
-        public static bool BatteryIsOk(float temperature, float soc, float chargeRate)
-        {
-            CheckTemperature(temperature);
-            CheckSoc(soc);
-            CheckChargeRate(chargeRate);
-            return (CheckTemperatureStatus(temperature) == StatusType.Normal &&
-                    CheckSocStatus(soc) == StatusType.Normal &&
-                    CheckChargeRateStatus(chargeRate) == StatusType.Normal);
+            return isBatteryOk;
         }
     }
 }
