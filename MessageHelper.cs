1586d467044d1f01033cdb039058ac0cd2b7b919
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 13:42:14 +0530

    Update MessageHelper.cs

--- MessageHelper.cs
+++ MessageHelper.cs
@@ -1,1 +1,45 @@
+namespace ParadigmShift
+{
+    public static class MessageHelper
+    {
+        public static string GetTemperatureMessage(StatusType status)
+        {
+            switch (status)
+            {
+                case StatusType.Warning:
+                    return GlobalSettings.AppLanguage == Language.English ? "Approaching temperature limit!" : "Annäherung an die Temperaturgrenze!";
+                case StatusType.Breach:
+                    return GlobalSettings.AppLanguage == Language.English ? "Temperature is out of range!" : "Temperatur ist außerhalb des Bereichs!";
+                default:
+                    return "";
+            }
+        }
+
+        public static string GetSocMessage(StatusType status)
+        {
+            switch (status)
+            {
+                case StatusType.Warning:
+                    return GlobalSettings.AppLanguage == Language.English ? "Approaching SOC limit!" : "Annäherung an die SOC-Grenze!";
+                case StatusType.Breach:
+                    return GlobalSettings.AppLanguage == Language.English ? "State of Charge is out of range!" : "Ladezustand ist außerhalb des Bereichs!";
+                default:
+                    return "";
+            }
+        }
+
+        public static string GetChargeRateMessage(StatusType status)
+        {
+            switch (status)
+            {
+                case StatusType.Warning:
+                    return GlobalSettings.AppLanguage == Language.English ? "Approaching charge rate limit!" : "Annäherung an die Ladegrenze!";
+                case StatusType.Breach:
+                    return GlobalSettings.AppLanguage == Language.English ? "Charge Rate is out of range!" : "Laderate ist außerhalb des Bereichs!";
+                default:
+                    return "";
+            }
+        }
+    }
+}
 
