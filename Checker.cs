fa9ef1f08795928756f459e1aedd8a2bd37e483f
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 15:21:49 +0530

    Update Checker.cs

--- Checker.cs
+++ Checker.cs
@@ -1,26 +1,109 @@
-using System.Collections.Generic;
+using System;
 
 namespace paradigm_shift_csharp
 {
     class Checker
     {
-        private List<Parameter> parameters = new List<Parameter>();
+        const float TempLowerLimit = 0;
+        const float TempUpperLimit = 45;
+        const float SocLowerLimit = 20;
+        const float SocUpperLimit = 80;
+        const float ChargeRateUpperLimit = 0.8f;
 
-        public void AddParameter(Parameter parameter)
+        const float TempWarningTolerance = TempUpperLimit * 0.05f;
+        const float SocWarningTolerance = SocUpperLimit * 0.05f;
+        const float ChargeRateWarningTolerance = ChargeRateUpperLimit * 0.05f;
+
+        static bool CheckTemperature(float temperature)
         {
-            parameters.Add(parameter);
+            bool status = true;
+            if (temperature < TempLowerLimit || temperature > TempUpperLimit)
+            {
+                Console.WriteLine("Temperature is out of range!");
+                status = false;
+            }
+            else
+            {
+                if (temperature <= TempLowerLimit + TempWarningTolerance)
+                {
+                    Console.WriteLine("Warning: Approaching discharge");
+                }
+                if (temperature >= TempUpperLimit - TempWarningTolerance)
+                {
+                    Console.WriteLine("Warning: Approaching charge-peak");
+                }
+            }
+            return status;
         }
 
-        public bool CheckBattery(params float[] values)
+        static bool CheckSoc(float soc)
         {
-            bool isBatteryOk = true;
-
-            for (int i = 0; i < values.Length; i++)
+            bool status = true;
+            if (soc < SocLowerLimit || soc > SocUpperLimit)
             {
-                isBatteryOk &= parameters[i].Check(values[i]);
+                Console.WriteLine("State of Charge is out of range!");
+                status = false;
             }
+            else
+            {
+                if (soc <= SocLowerLimit + SocWarningTolerance)
+                {
+                    Console.WriteLine("Warning: Approaching discharge");
+                }
+                if (soc >= SocUpperLimit - SocWarningTolerance)
+                {
+                    Console.WriteLine("Warning: Approaching charge-peak");
+                }
+            }
+            return status;
+        }
 
-            return isBatteryOk;
+        static bool CheckChargeRate(float chargeRate)
+        {
+            bool status = true;
+            if (chargeRate > ChargeRateUpperLimit)
+            {
+                Console.WriteLine("Charge Rate is out of range!");
+                status = false;
+            }
+            else
+            {
+                if (chargeRate >= ChargeRateUpperLimit - ChargeRateWarningTolerance)
+                {
+                    Console.WriteLine("Warning: Approaching charge-peak");
+                }
+            }
+            return status;
+        }
+
+        static bool batteryIsOk(float temperature, float soc, float chargeRate)
+        {
+            return CheckTemperature(temperature) && CheckSoc(soc) && CheckChargeRate(chargeRate);
+        }
+
+        static void ExpectTrue(bool expression)
+        {
+            if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                Environment.Exit(1);
+            }
+        }
+        static void ExpectFalse(bool expression)
+        {
+            if (expression)
+            {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+            }
+        }
+
+        static int Main()
+        {
+            ExpectTrue(batteryIsOk(25, 70, 0.7f));
+            ExpectFalse(batteryIsOk(50, 85, 0.0f));
+            Console.WriteLine("All ok");
+            return 0;
         }
     }
 }
