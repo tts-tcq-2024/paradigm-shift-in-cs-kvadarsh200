06ce24dd49527359320daa795b74781d21169d31
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 13:58:19 +0530

    Update and rename MessageHelper.cs to Parameter.cs

---
+++ Parameter.cs
@@ -0,0 +1,42 @@
+using ParadigmShift;
+
+namespace ParadigmShift
+{
+    
+class Parameter
+    {
+        private readonly string name;
+        private readonly float lowerLimit;
+        private readonly float upperLimit;
+        private readonly float warningTolerance;
+        private readonly BatteryStatus batteryStatus;
+
+        public Parameter(string name, float lowerLimit, float upperLimit, float warningTolerance, BatteryStatus batteryStatus)
+        {
+            this.name = name;
+            this.lowerLimit = lowerLimit;
+            this.upperLimit = upperLimit;
+            this.warningTolerance = warningTolerance;
+            this.batteryStatus = batteryStatus;
+        }
+
+        public bool Check(float value)
+        {
+            if (value < lowerLimit || value > upperLimit)
+            {
+                batteryStatus.AddMessage(Localization.GetMessage($"{name}_BREACH"));
+                return false;
+            }
+            else if (value < lowerLimit + warningTolerance)
+            {
+                batteryStatus.AddMessage(Localization.GetMessage($"{name}_APPROACHING_DISCHARGE"));
+            }
+            else if (value > upperLimit - warningTolerance)
+            {
+                batteryStatus.AddMessage(Localization.GetMessage($"{name}_APPROACHING_PEAK"));
+            }
+
+            return true;
+        }
+    }
+}
