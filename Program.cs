ec6e64f1b5d591c28cb2f3322ad38c4dffbe7d7f
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-08-05 14:51:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,6 @@ namespace paradigm_shift_csharp
 {
     class Program
     {
-        static void Main()
-        {
-            Checker checker = new Checker();
-            ExpectTrue(checker.BatteryIsOk(25, 70, 0.7f));
-            ExpectFalse(checker.BatteryIsOk(50, 85, 0.0f));
-            Console.WriteLine("All ok");
-        }
-
         static void ExpectTrue(bool expression)
         {
             if (!expression)
@@ -28,6 +20,17 @@ namespace paradigm_shift_csharp
                 Console.WriteLine("Expected false, but got true");
                 Environment.Exit(1);
             }
+        }
+
+        static int Main()
+        {
+            var logger = new TestWarningLogger();
+            var checker = new Checker(logger);
+
+            ExpectTrue(checker.BatteryIsOk(25, 70, 0.7f));
+            ExpectFalse(checker.BatteryIsOk(50, 85, 0.0f));
+            Console.WriteLine("All ok");
+            return 0;
         }
     }
 }
