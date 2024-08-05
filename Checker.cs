2cc2db2af04af343ef5f0ea2251bd38a16e48363
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-08-05 14:47:39 +0530

    Update Checker.cs

--- Checker.cs
+++ Checker.cs
@@ -1,22 +1,44 @@
 using System;
 namespace paradigm_shift_csharp
 {
-     class Checker
+
+    public class Checker
     {
-        private readonly RangeChecker _rangeChecker;
-        private readonly Notifier _notifier;
-        
-        public Checker()
+        private readonly IWarningLogger _logger;
+
+        public Checker(IWarningLogger logger)
         {
-            _rangeChecker = new RangeChecker();
-            _notifier = new Notifier();
+            _logger = logger;
+        }
+
+        public bool CheckTemperature(float temperature)
+        {
+            bool status = !RangeChecker.IsOutOfRange(temperature, Constants.TempLowerLimit, Constants.TempUpperLimit);
+            WarningPrinter.PrintWarning(_logger, temperature, Constants.TempLowerLimit, Constants.TempUpperLimit, Constants.TempWarningTolerance, "Temperature");
+            return status;
+        }
+
+        public bool CheckSoc(float soc)
+        {
+            bool status = !RangeChecker.IsOutOfRange(soc, Constants.SocLowerLimit, Constants.SocUpperLimit);
+            WarningPrinter.PrintWarning(_logger, soc, Constants.SocLowerLimit, Constants.SocUpperLimit, Constants.SocWarningTolerance, "State of Charge");
+            return status;
+        }
+
+        public bool CheckChargeRate(float chargeRate)
+        {
+            bool status = chargeRate <= Constants.ChargeRateUpperLimit;
+            if (chargeRate >= Constants.ChargeRateUpperLimit - Constants.ChargeRateWarningTolerance)
+            {
+                _logger.LogWarning("Warning: Charge Rate approaching charge-peak");
+            }
+            return status;
         }
 
         public bool BatteryIsOk(float temperature, float soc, float chargeRate)
         {
-            return _rangeChecker.Check(temperature, Constants.TempLowerLimit, Constants.TempUpperLimit, "Temperature") &&
-                   _rangeChecker.Check(soc, Constants.SocLowerLimit, Constants.SocUpperLimit, "State of Charge") &&
-                   _rangeChecker.CheckChargeRate(chargeRate);
+            return CheckTemperature(temperature) && CheckSoc(soc) && CheckChargeRate(chargeRate);
         }
     }
+
 }
