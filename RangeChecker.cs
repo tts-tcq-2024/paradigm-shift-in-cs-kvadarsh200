3e4f6a0fbac1595fa6527b2dc1a9d3459bd16e3a
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-08-05 14:48:14 +0530

    Update RangeChecker.cs

--- RangeChecker.cs
+++ RangeChecker.cs
@@ -2,37 +2,11 @@ using System;
 
 namespace paradigm_shift_csharp
 {
-    class RangeChecker
+    public static class RangeChecker
     {
-        private readonly Notifier _notifier;
-        public RangeChecker()
+        public static bool IsOutOfRange(float value, float lowerLimit, float upperLimit)
         {
-            _notifier = new Notifier();
-        }
-
-        public bool Check(float value, float lowerLimit, float upperLimit, string parameterName)
-        {
-            if (value.IsOutOfRange(lowerLimit, upperLimit))
-            {
-                Console.WriteLine($"{parameter} is out of range!");
-                return false;
-            }
-            _notifier.PrintWarning(value, lowerLimit, upperLimit, parameterName);
-            return true;
-        }
-
-        public bool CheckChargeRate(float chargeRate)
-        {
-            if (chargeRate > Constants.ChargeRateUpperLimit)
-            {
-                Console.WriteLine("Charge Rate is out of range!");
-                return false;
-            }
-            if (chargeRate >= Constants.ChargeRateUpperLimit - Constants.ChargeRateWarningTolerance)
-            {
-                Console.WriteLine("Warning: Approaching charge-peak");
-            }
-            return true;
+            return value < lowerLimit || value > upperLimit;
         }
     }
 }
