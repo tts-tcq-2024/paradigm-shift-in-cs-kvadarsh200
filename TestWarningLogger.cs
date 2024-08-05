71131eddc79509ecfe3c55d5c67c85cb016bb41b
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-08-05 14:56:05 +0530

    Create TestWarningLogger.cs

---
+++ TestWarningLogger.cs
@@ -0,0 +1,10 @@
+namespace paradigm_shift_csharp
+{
+   public class TestWarningLogger : IWarningLogger
+    {
+        public void LogWarning(string message)
+        {
+            // For testing purposes, we can just collect the warnings or use assertions
+        }
+    }
+}
